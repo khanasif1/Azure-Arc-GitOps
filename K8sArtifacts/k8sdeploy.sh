2638f2fedcff453c5fe49410312e28818f0172dd
khanasif1 <khanasif1@gmail.com>
2021-09-13 23:22:58 +1000

    update

--- K8sArtifacts/k8sdeploy.sh
+++ K8sArtifacts/k8sdeploy.sh
@@ -4,11 +4,13 @@ sudo apt install wget
 
 kubectl config set-context --current --namespace=$namespace
 
-wget -O - https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/AKSArtifacts/IngressController/Ingress.sh | sudo bash
+wget -O - https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/K8sArtifacts/IngressController/Ingress.sh | sudo bash
+
+#************Apps************
+
+#microk8s kubectl apply -f https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/K8sArtifacts/yaml/web.yaml
 
 #*******Ingress Controller*********
 
-#microk8s kubectl apply -f https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/AKSArtifacts/IngressController/ingress.yaml 
-#************Apps************
+#microk8s kubectl apply -f https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/K8sArtifacts/IngressController/ingress.yaml 
 
-#microk8s kubectl apply -f https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/AKSArtifacts/yaml/web.yaml
