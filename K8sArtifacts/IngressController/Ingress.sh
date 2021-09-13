8498320bbceeb3aa04dd862fdf7f081db935942a
khanasif1 <khanasif1@gmail.com>
2021-09-13 23:31:46 +1000

    update

--- K8sArtifacts/IngressController/Ingress.sh
+++ K8sArtifacts/IngressController/Ingress.sh
@@ -1,5 +1,6 @@
 # Use Helm to deploy an NGINX ingress controller
-helm repo update
+#helm repo update
 #helm install nginx-ingress stable/nginx-ingress --namespace arc-k8-org --set controller.replicaCount=1 --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
-
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
 helm install nginx-ingress ingress-nginx/ingress-nginx
