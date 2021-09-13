7cad3cc388715d8c86af1c543a1ea791a1680ad3
khanasif1 <khanasif1@gmail.com>
2021-09-13 21:56:26 +1000

    updated

--- AKSArtifacts/k8sdeploy.ps1
+++ AKSArtifacts/k8sdeploy.ps1
@@ -1,11 +1,11 @@
-$namespace="k8rg"
+$namespace="arc-k8-org"
 
 
-kubectl config set-context --current --namespace=**namespace**
+kubectl config set-context --current --namespace=$namespace
 
 <#*******Ingress Controller*********#>
 
-kubectl apply -f .\IngressController\ingress.yaml
+kubectl apply -f https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/AKSArtifacts/IngressController/ingress.yaml
 <#************Apps************#>
 
-kubectl apply -f yaml/web.yaml
+kubectl apply -f https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/AKSArtifacts/yaml/web.yaml
