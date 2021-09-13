88742b1a2094d88b85a49cf22b46bd846234f0a1
khanasif1 <khanasif1@gmail.com>
2021-09-13 23:19:33 +1000

    update

--- K8sArtifacts/k8sdeploy.sh
+++ K8sArtifacts/k8sdeploy.sh
@@ -6,9 +6,9 @@ kubectl config set-context --current --namespace=$namespace
 
 wget -O - https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/AKSArtifacts/IngressController/Ingress.sh | sudo bash
 
-<#*******Ingress Controller*********#>
+!#*******Ingress Controller*********
 
 #microk8s kubectl apply -f https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/AKSArtifacts/IngressController/ingress.yaml 
-<#************Apps************#>
+#************Apps************
 
 #microk8s kubectl apply -f https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/AKSArtifacts/yaml/web.yaml
