d6a854b7522fd95deeaf58e50c7dfdc5816083e0
khanasif1 <khanasif1@gmail.com>
2021-09-13 23:28:21 +1000

    update

--- K8sArtifacts/cleanup/delete.sh
+++ K8sArtifacts/cleanup/delete.sh
@@ -1,6 +1,6 @@
 kubectl config set-context --current --namespace=arc-k8-org
-<#
-========DELETE============
+
+#========DELETE============
 microk8s kubectl delete ns arc-k8-org
 
 helm delete nginx-ingress
