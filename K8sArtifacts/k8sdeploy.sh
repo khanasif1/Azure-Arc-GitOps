deb3a9f3c2d35414729e647a958c4987ec21b237
khanasif1 <khanasif1@gmail.com>
2021-09-13 23:41:00 +1000

    u

--- K8sArtifacts/k8sdeploy.sh
+++ K8sArtifacts/k8sdeploy.sh
@@ -1,11 +1,12 @@
 $namespace="arc-k8-org"
 
-sudo apt install wget
+#sudo apt install wget
 
-kubectl config set-context --current --namespace=$namespace
+#kubectl config set-context --current --namespace=$namespace
 
-wget -O - https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/K8sArtifacts/IngressController/Ingress.sh | sudo bash
+#wget -O - https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/K8sArtifacts/IngressController/Ingress.sh | sudo bash
 
+microk8s enable ingress
 #************Apps************
 
 #microk8s kubectl apply -f https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/K8sArtifacts/yaml/web.yaml
