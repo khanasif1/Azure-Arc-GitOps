390661fb418651e7407f8fb012fd5a8396a9715d
khanasif1 <khanasif1@gmail.com>
2021-09-15 13:58:31 +1000

    update

--- K8sArtifacts/k8sdeploy.sh
+++ K8sArtifacts/k8sdeploy.sh
@@ -2,13 +2,24 @@
 
 #kubectl config set-context --current --namespace=$namespace
 echo "*****Installing Ingress******"
-wget -O - https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/K8sArtifacts/IngressController/Ingress.sh | sudo bash
+
+outns=$(microk8s kubectl get ns -o=name) \
+for x in $outns \
+    do \
+        echo "$x"
+        if [[ "$x" = "namespace/ingress" ]] \
+        then  \
+            echo "match" \
+            wget -O - https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/K8sArtifacts/IngressController/Ingress.sh | sudo bash
+        else \
+            echo "Ingress already exists!" \
+        fi \
+    done
 
 #************Apps************
 echo "*****Deploy Web Application******"
-#microk8s kubectl apply -f https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/K8sArtifacts/yaml/web.yaml
+microk8s kubectl apply -f https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/K8sArtifacts/yaml/web.yaml
 
 #*******Ingress Controller*********
 echo "*****Deploy Ingress Routing******"
 #microk8s kubectl apply -f https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/K8sArtifacts/IngressController/ingress.yaml 
-
