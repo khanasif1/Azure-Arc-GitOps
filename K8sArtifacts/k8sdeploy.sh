a9510346a6f99c714d2fd67228ace0cdfe062e2a
khanasif1 <khanasif1@gmail.com>
2021-09-15 14:03:28 +1000

    update

--- K8sArtifacts/k8sdeploy.sh
+++ K8sArtifacts/k8sdeploy.sh
@@ -3,17 +3,17 @@
 #kubectl config set-context --current --namespace=$namespace
 echo "*****Installing Ingress******"
 
-outns=$(microk8s kubectl get ns -o=name) \
-for x in $outns \
-    do \
-        echo "$x" \
-        if [[ "$x" = "namespace/ingress" ]] \
-        then  \
-            echo "match" \
+outns=$(microk8s kubectl get ns -o=name) `
+for x in $outns 
+    do 
+        echo "$x" 
+        if [[ "$x" = "namespace/ingress" ]] 
+        then  
+            echo "match" 
             wget -O - https://raw.githubusercontent.com/khanasif1/Azure-Arc-GitOps/main/K8sArtifacts/IngressController/Ingress.sh | sudo bash
-        else \
-            echo "Ingress already exists!" \
-        fi \
+        else 
+            echo "Ingress already exists!" 
+        fi 
     done
 
 #************Apps************
