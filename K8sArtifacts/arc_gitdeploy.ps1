1f56f3da2f89072bd56ae0a86c167003a31b3968
khanasif1 <khanasif1@gmail.com>
2021-09-21 17:21:14 +1000

    update

--- K8sArtifacts/arc_gitdeploy.ps1
+++ K8sArtifacts/arc_gitdeploy.ps1
@@ -26,6 +26,16 @@ az k8s-configuration create `
   --operator-params="--git-branch=main --git-poll-interval 3s --git-readonly --git-path=K8sArtifacts/yaml"
 
 
+  $ns="arc-k8"
+  az k8s-configuration create `
+ --name arc-web1 `
+ --cluster-name $arcClusterName --resource-group $resourceGroup `
+  --operator-instance-name arc-web  --operator-namespace $ns `
+  --repository-url $appClonedRepo `
+  --scope namespace   --cluster-type connectedClusters `
+  --operator-params="--git-branch=main --git-poll-interval 3s --git-readonly --git-path=K8sArtifacts/yaml/app2"
+
+
 ###########################################################################
 ######################Validate Config################################
 az k8s-configuration show --name arc-web1 --cluster-name $arcClusterName --resource-group $resourceGroup --cluster-type connectedClusters
@@ -37,6 +47,10 @@ az k8s-configuration show --name arc-web1 --cluster-name $arcClusterName --resou
 
 k delete ns arc-k8-org
 k create ns arc-k8-org
+
+k delete ns arc-k8
+k create ns arc-k8
+
 
 k -n arc-k8-org get deploy  -o wide
 k get pod -n  arc-k8-org -o wide
